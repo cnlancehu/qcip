a0b9317b15e20cdb684421c22aa665ca5fbb9420
cnlancehu <2124757129@qq.com>
2023-08-08 21:46:10 +0800

    chore: better output

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 	} else {
 		for i, arg := range os.Args {
 			if arg == "-h" || arg == "--help" {
-				fmt.Printf("QCIP \033[1;32mv%s\033[0m\nUsuage:	qcip [options] [<value>]\nOptions:\n	-c, --config <path>	Specify the location of the configuration file and run\n	-v, --version	Show version information\n	-h, --help	Show this help page\nExamples:\n	\033[33mqcip\033[0m	Run the program with config.json\n	\033[33mqcip -c qcipconf.json\033[0m	Specify to use the configuration file qcipconf.json and run the program\nVisit our Github repo for more helps\n	https://github.com/cnlancehu/qcip", version)
+				fmt.Printf("QCIP \033[1;32mv%s\033[0m\nUsuage:	qcip [options] [<value>]\nOptions:\n  -c, --config <path>	Specify the location of the configuration file and run\n  -v, --version		Show version information\n  -h, --help		Show this help page\nExamples:\n  \033[33mqcip\033[0m	Run the program with config.json\n  \033[33mqcip -c qcipconf.json\033[0m	Specify to use the configuration file qcipconf.json and run the program\nVisit our Github repo for more helps\n  https://github.com/cnlancehu/qcip", version)
 				return
 			} else if arg == "-v" || arg == "--version" {
 				showversion()
@@ -147,7 +147,7 @@ func showversion() {
 	vernow, _ := strconv.Atoi(strings.Replace(version, ".", "", -1))
 	verlatest, _ := strconv.Atoi(strings.Replace(latestver, ".", "", -1))
 	if verlatest > vernow {
-		fmt.Printf("\rNew version available: \033[1;32m%s\033[0m\nDownload it here: \n 	https://github.com/cnlancehu/qcip/releases/tag/%s", latestver, latestver)
+		fmt.Printf("\rNew version available: \033[1;32m%s\033[0m\nDownload it here: \n  https://github.com/cnlancehu/qcip/releases/tag/%s", latestver, latestver)
 	} else {
 		fmt.Printf("\r\033[1;32mYou are using the latest version\033[0m\n")
 	}
